38b91767289936d6225d4eabc66f80bebc6de563
Panagiotis Tigas <ptigas@gmail.com>
2014-06-15 18:15:46 +0100

    added database

---
+++ webapp/database.sql
@@ -0,0 +1,12 @@
+SET character_set_client = utf8;
+
+USE antihoax;
+
+DROP TABLE IF EXISTS hoax;
+
+CREATE TABLE hoax (
+	id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	url VARCHAR(255), 
+	evidence TEXT,
+	PRIMARY KEY (id)
+ ) ENGINE=MyISAM;
