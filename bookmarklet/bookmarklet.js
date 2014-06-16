18e4365d2d6991a154fd24468dabadf5b978a99d
Panagiotis Tigas <ptigas@gmail.com>
2014-06-16 22:30:23 +0100

    send url as base64

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,6 +1,6 @@
 (function(){	
 	var script = document.createElement("script");
-	script.src = "http://localhost/antihoax/api.php?f=js&u="+document.URL;
+	script.src = "http://localhost/antihoax/api.php?f=js&u="+window.btoa(document.URL);
 	script.onload = script.onreadystatechange = function(){
 		if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
 			done = true;
