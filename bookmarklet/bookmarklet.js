60240a1d28e171d2d4cf426a1a058e6c77915764
Panagiotis Tigas <ptigas@gmail.com>
2014-06-16 22:07:14 +0100

    keep it simple :)

--- bookmarklet/bookmarklet.js
+++ bookmarklet/bookmarklet.js
@@ -1,31 +1,11 @@
 (function(){
-
-	// the minimum version of jQuery we want
-	var v = "1.3.2";
-
-	// check prior inclusion and version
-	if (window.jQuery === undefined || window.jQuery.fn.jquery < v) {
-		var done = false;
-		var script = document.createElement("script");
-		script.src = "http://ajax.googleapis.com/ajax/libs/jquery/" + v + "/jquery.min.js";
-		script.onload = script.onreadystatechange = function(){
-			if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
-				done = true;
-				initMyBookmarklet();
-			}
-		};
-		document.getElementsByTagName("head")[0].appendChild(script);
-	} else {
-		initMyBookmarklet();
-	}
-	
-	function initMyBookmarklet() {
-		(window.myBookmarklet = function() {
-			// your JavaScript code goes here!
-			$(document.body).append('
-    			<iframe id="results" style="background:#eee; padding: 0px; position: absolute; top: 10px; right: 10px; z-index: 999999999;" frameborder="0" scrolling="no" width="350px" height="660px"></iframe>
-			');
-		})();
-	}
-
+	var src = "http://localhost/antihoax/api.php?f=js&u="+url;
+	var script = document.createElement("script");
+	script.src = src;
+	script.onload = script.onreadystatechange = function(){
+		if (!done && (!this.readyState || this.readyState == "loaded" || this.readyState == "complete")) {
+			done = true;
+		}
+	};
+	document.getElementsByTagName("head")[0].appendChild(script);
 })();
