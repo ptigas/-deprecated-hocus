d4690a41dfe9284ff9d585220bc6bd2c3db15e7c
Panagiotis Tigas <ptigas@gmail.com>
2015-07-22 10:14:16 +0100

    added sidebar view

--- webapp/api.php
+++ webapp/api.php
@@ -11,12 +11,26 @@ if (isset($_GET['u']))
 	$is_hoax = $hoax !== null;
 }
 
-if (isset($_GET['f']) && $_GET['f'] == 'js') {
-	echo $twig->render('bookmarklet.js', array(
-		'url' => $url,
-		'is_hoax' => $is_hoax ? 'true':'false'
-		)
-	);
+if (isset($_GET['f'])){
+	switch ($_GET['f']) {
+		case 'js':
+			echo $twig->render('bookmarklet.js', array(
+					'url' => $url,
+					'is_hoax' => $is_hoax ? 'true':'false'
+				));
+			break;
+		case 'sidebar':
+			
+			if ($hoax) 
+			{
+				echo $hoax->evidence;
+			}			
+
+			break;
+		default:
+			# code...
+			break;
+	}	
 } else {
 	echo json_encode($hoax);
 }
