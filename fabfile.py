a02cbf8b197a74d51803ef6bf5f40794e1a59bfc
Panagiotis Tigas <ptigas@gmail.com>
2016-09-18 23:41:12 +0300

    changed domain

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,16 @@
 from fabric.api import *
 
 env.user = 'ptigas'
-env.hosts = ['ptigas.com']
+env.hosts = ['hocus.io']
 
 def deploy():
 	with cd('repos/hocus'):
-		run('git pull')				
+		run('git pull')
 
 	with cd('repos/hocus/webapp'):
 		run('composer install')
 
 	with cd('repos/hocus'):
-		run('cp -r webapp/* ~/Sites/hocus.io/')	
+		run('cp -r webapp/* ~/Sites/hocus.io/')
 		run('cp -r vendor ~/Sites/hocus.io/.')
 		run('cp -r ~/Sites/hocus.io/settings.prod.php ~/Sites/hocus.io/settings.php')
