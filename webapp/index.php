e3f1e36c241821b2e47df5fff9f5c2fb66ab62ed
Panagiotis Tigas <ptigas@gmail.com>
2016-09-20 22:12:25 +0300

    migrated the routes

--- webapp/index.php
+++ webapp/index.php
@@ -2,6 +2,109 @@
 
 include 'core.php';
 
+$app = new \Slim\App();
+
+// Main
+$app->get('/', function ($request, $response, $args) {
+    global $twig;
+    global $base;
+
+    return $twig->render('index.html', array( 
+      'base' => $base,      
+      'hoaxes' => 103
+    ));
+});
+
+$app->get('/u/{url}/', function ($request, $response, $args) {
+    global $twig;
+
+    $url = $request->getAttribute('url');
+    var_dump($url);
+});
+
+$app->post('/u/{url}/', function ($request, $response, $args) {
+    global $twig;
+
+    $url = $request->getAttribute('url');
+    var_dump($url);
+});
+
+$app->get('/edit/i/{id}/', function ($request, $response, $args) {
+    global $twig;
+    global $base;
+
+    $hoax = ORM::for_table('hoax', 'remote')->find_one($request->getAttribute('id'));
+    $id = $hoax->id;
+    $url = $hoax->url;    
+    $evidence = $hoax->evidence;
+    
+    return  $twig->render('edit.html', array( 
+      'base' => $base,
+      'url' => $url,
+      'evidence' => $evidence,
+      'alert' => $alert,
+      'id' => $id
+    ));
+});
+
+$app->get('/edit/u/{url}/', function ($request, $response, $args) {
+    global $twig;
+    global $base;
+
+    $url = base64_decode($request->getAttribute('url'));
+
+    $hoax = ORM::for_table('hoax', 'remote')->where('url', $url)->find_one();
+
+    $id = -1;
+    $evidence = "";
+    if ($hoax) {
+      $id = $hoax->id;
+      $url = $hoax->url;  
+      $evidence = $hoax->evidence;
+    }
+    
+    return  $twig->render('edit.html', array( 
+      'base' => $base,
+      'url' => $url,
+      'evidence' => $evidence,
+      'alert' => $alert,
+      'id' => $id
+    ));
+});
+
+$app->get('/api/u/{url}/{type}', function ($request, $response, $args) {
+    global $twig;
+
+    $url = \Hocus\Normalizer::normalize_url(base64_decode($request->getAttribute('url')));
+    $hoax = Hoax::fetch_hoax($url);
+    $is_hoax = $hoax !== null;
+
+    $type = $request->getAttribute('type');
+    switch ($type) {
+    case 'js':
+      return $twig->render('bookmarklet.js', array(
+          'url' => $url,          
+          'is_hoax' => $is_hoax ? 'true':'false'
+        ));
+      break;
+    case 'sidebar':      
+      return $twig->render('sidebar.html', array(
+          'url' => $url,
+          'url_base64' => base64_encode($url),
+          'evidence' => $hoax ? $hoax->evidence : ''
+        ));     
+
+      break;
+    case 'json':
+      return json_encode($hoax);
+    default:
+      # code...
+      break;
+  } 
+});
+
+$app->run();
+/*
 $id = -1;
 $url = '';
 $evidence = '';
@@ -41,6 +144,6 @@ echo $twig->render('index.html', array(
   'url' => $url,
   'hoaxes' => 102
   ));
-
+*/
 ?>
 
